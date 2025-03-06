30c297dfa656cfb3b2ea2b6463c6e1c0d90833da
Nazarko-lv <99681751+Nazarko-lv@users.noreply.github.com>
2025-03-06 19:58:41 +0200

    Add files via upload

--- lesson_18/js/script.js
+++ lesson_18/js/script.js
@@ -3,7 +3,7 @@
 document.addEventListener("click", function (e) {
 	const targetElement = e.target;
 	// .icon-menu це клас кнопки при кліку на яку буде спрацьовувати код додаватись до тегу html клас menu-open 
-	if (targetElement.closest('.burger-icon')) {
+	if (targetElement.closest('.icon-menu')) {
 		document.documentElement.classList.toggle('open-menu');
 		e.preventDefault();
 	}
